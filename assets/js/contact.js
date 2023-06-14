dfca383295d09b05958cc8edc16b348079e9894f
Kassandra Dominguez <kassdomlara02@gmail.com>
2023-06-14 16:36:09 -0600

    Se agregan cambios y correcciones los archivos de contacto. Nav var y footer estan integrados

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,24 +9,29 @@ formularioContacto.addEventListener("submit", validarFormulario);
 //3.- la funcion que queremos que se ejecute cuando ocurra el evento:
 
 function validarFormulario(event) {
-    
+
     event.preventDefault();
 
     const nombre = document.querySelector("#nombreContacto").value;
 
     const email = document.querySelector("#emailContacto").value;
 
+    const telefono = document.querySelector("#telefonoContacto").value;
+
     const asunto = document.querySelector("#asuntoContacto").value;
 
+    //Evaluamos que ninguno de los campos del formulario se quede vacio
 
-
+    //validacion de nombre
 
     if (nombre.length == 0) {
 
         alert("Es necesario ingresar un nombre");
-        
+
         return;
     }
+
+    // validacion de correo
 
     if (email.length == 0) {
 
@@ -34,15 +39,34 @@ function validarFormulario(event) {
         return
     }
 
+    // Evaluamos el dato de telefono usando RegExp
+
+    // declaramos una funcion que devolvera un valor booleano:
+
+    function validarTelefono(telefono) {
+        const regex = /^\d{10}$/; // Expresión regular para validar un número de teléfono de 10 dígitos
+        return regex.test(telefono);
+    }
+
+    if (validarTelefono(telefono)) {
+
+        alert("el numero es correcto");
+
+    } else { alert("El dato que ingreso no es un numero de telefono"); }
+
+
+    //validacion del mensaje
+
     if (asunto.length == 0) {
         alert("Es necesario ingresar un mensaje");
         return
     }
 
-
     alert("tu mensanje se ha enviado con exito");
 
 
-
+    console.log(telefono);
 
 }
+
+
