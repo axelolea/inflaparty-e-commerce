50ec59f73fbcbcc7441dcb9d71fc43f3d4cf6f2b
Yeimi Magadan <mcyeimi99@gmail.com>
2023-07-01 14:34:22 -0600

    Se agrega cambios para subir imagen de perfil se usuario

--- assets/js/account.js
+++ assets/js/account.js
@@ -34,41 +34,53 @@ import { endpoints } from './constants.js'
 import httpClient from './httpClient.js'
 import sessionManagement from './sessionManagement.js'
 
-
 window.addEventListener('DOMContentLoaded', (event) => {
-    const productSection = document.getElementById('product-section');
-  
-    const products = ['Producto 1', 'Producto 2', 'Producto 3']; //esto se coloca como demo únicamente
-  
-    //Condición para productos
-    if (products.length > 0) {
-      products.forEach((product) => {
-        const productItem = document.createElement('p');
-        productItem.textContent = product;
-        productSection.appendChild(productItem);
-      });
-    } else {
-     
-      const noProductsMessage = document.createElement('p');
-      noProductsMessage.textContent = 'No hay productos disponibles.';
-      productSection.appendChild(noProductsMessage);
-    }
-  
-    //Cómo obtener el contenedor de reservas?
-    const reservationSection = document.getElementById('reservation-section');
+  const productSection = document.getElementById('product-section');
 
-    const reservations = ['Reserva 1', 'Reserva 2', 'Reserva 3'];  //esto se coloca como demo únicamente
-  
-    //Condición para reservas
-    if (reservations.length > 0) {
-      reservations.forEach((reservation) => {
-        const reservationItem = document.createElement('p');
-        reservationItem.textContent = reservation;
-        reservationSection.appendChild(reservationItem);
-      });
-    } else {
-      const noReservationsMessage = document.createElement('p');
-      noReservationsMessage.textContent = 'No hay reservas disponibles.';
-      reservationSection.appendChild(noReservationsMessage);
-    }
+  const products = ['Producto 1', 'Producto 2', 'Producto 3']; // Esto es solo para demostración
+
+  // Condición para productos
+  if (products.length > 0) {
+    products.forEach((product) => {
+      const productItem = document.createElement('p');
+      productItem.textContent = product;
+      productSection.appendChild(productItem);
+    });
+  } else {
+    const noProductsMessage = document.createElement('p');
+    noProductsMessage.textContent = 'No hay productos disponibles.';
+    productSection.appendChild(noProductsMessage);
+  }
+
+  const reservationSection = document.getElementById('reservation-section');
+
+  const reservations = ['Reserva 1', 'Reserva 2', 'Reserva 3']; // Esto es solo para demostración
+
+  // Condición para reservas
+  if (reservations.length > 0) {
+    reservations.forEach((reservation) => {
+      const reservationItem = document.createElement('p');
+      reservationItem.textContent = reservation;
+      reservationSection.appendChild(reservationItem);
+    });
+  } else {
+    const noReservationsMessage = document.createElement('p');
+    noReservationsMessage.textContent = 'No hay reservas disponibles.';
+    reservationSection.appendChild(noReservationsMessage);
+  }
+
+  const fotoPerfilInput = document.getElementById('fotoPerfilInput');
+  const fotoPerfilPreview = document.getElementById('fotoPerfil');
+
+  //para agregar la foto de perfil
+  fotoPerfilInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      fotoPerfilPreview.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
   });
+});
