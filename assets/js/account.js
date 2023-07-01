a1961f9cef11be1986321dbc23b8dc3796768fd9
Yeimi Magadan <mcyeimi99@gmail.com>
2023-07-01 12:50:04 -0600

    Se agregan cambios a página de perfil de usuario

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,8 +1,9 @@
-import { endpoints } from './constants.js'
+/*import { endpoints } from './constants.js'
 import httpClient from './httpClient.js'
 import sessionManagement from './sessionManagement.js'
 
-const formElement = document.querySelector("#accountForm")
+
+(const formElement = document.querySelector("#accountForm")
 const accountForm = new FormData(formElement)
 
 const data = {
@@ -19,10 +20,55 @@ Object.entries(data).forEach(item => {
     accountForm.set(key, value)
 })
 
-console.log(accountForm.entries())
+console.log(accountForm.entries()) */
 
 // const token = sessionManagement.getToken()
 
 // httpClient.get(endpoints.getUsuario, { token })
 //     .then(resp => console.log(resp))
-//     .catch(err => console.log(err))
+//     .catch(err => console.log(err)) 
+
+//Reusar código para 
+
+import { endpoints } from './constants.js'
+import httpClient from './httpClient.js'
+import sessionManagement from './sessionManagement.js'
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const productSection = document.getElementById('product-section');
+  
+    const products = ['Producto 1', 'Producto 2', 'Producto 3']; //esto se coloca como demo únicamente
+  
+    //Condición para productos
+    if (products.length > 0) {
+      products.forEach((product) => {
+        const productItem = document.createElement('p');
+        productItem.textContent = product;
+        productSection.appendChild(productItem);
+      });
+    } else {
+     
+      const noProductsMessage = document.createElement('p');
+      noProductsMessage.textContent = 'No hay productos disponibles.';
+      productSection.appendChild(noProductsMessage);
+    }
+  
+    //Cómo obtener el contenedor de reservas?
+    const reservationSection = document.getElementById('reservation-section');
+
+    const reservations = ['Reserva 1', 'Reserva 2', 'Reserva 3'];  //esto se coloca como demo únicamente
+  
+    //Condición para reservas
+    if (reservations.length > 0) {
+      reservations.forEach((reservation) => {
+        const reservationItem = document.createElement('p');
+        reservationItem.textContent = reservation;
+        reservationSection.appendChild(reservationItem);
+      });
+    } else {
+      const noReservationsMessage = document.createElement('p');
+      noReservationsMessage.textContent = 'No hay reservas disponibles.';
+      reservationSection.appendChild(noReservationsMessage);
+    }
+  });
